0a07056950ac15f37cd219a90098a07f436f961c
Kerolos Fayez <kerofaiz1@gmail.com>
2023-05-14 21:19:12 +0300

    voting panel JS Edited by Matthew

--- pages/voting_panel/votingpanel.js
+++ pages/voting_panel/votingpanel.js
@@ -1,18 +1,14 @@
 let voteBtn = document.querySelectorAll("#myButton");
 let voteCounter = 0;
-
-// To get backgroundColor of button
-let btn = document.getElementById("myButton");
 // get all styles of the button
-let mainstyle = getComputedStyle(btn);
+let mainstyle = getComputedStyle(document.getElementById("myButton"));
 // get backgroundColor and 
 let mainbackcolor = mainstyle.backgroundColor;
 
 voteBtn.forEach(function (e) {
   e.onclick = function () {
     let styles = getComputedStyle(this);
-    let backgroundColor = styles.backgroundColor;
-    if (backgroundColor === mainbackcolor) {
+    if (styles.backgroundColor === mainbackcolor) {
       if (voteCounter < 2) {
         this.style.backgroundColor = "green";
         voteCounter++;
