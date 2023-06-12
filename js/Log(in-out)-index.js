97470c9df00b10cb0bced311475f84076569bdbe
Matthew-19 <113844755+Matthew-19@users.noreply.github.com>
2023-06-12 18:58:25 +0300

    Register button
    register button disappears after logging in

--- js/Log(in-out)-index.js
+++ js/Log(in-out)-index.js
@@ -1,10 +1,13 @@
 let mylogIndex = document.getElementById('logIndex');
+let register = document.getElementById('register');
 
 if (localStorage.getItem("token")) {
   mylogIndex.innerHTML =`Logout`;
   mylogIndex.href = 'index.html';
+  register.style.display = "none";
 }else {
   mylogIndex.innerHTML =`Login`;
+  register.style.display = "block";
 }
 
 function logout() {
