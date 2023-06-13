2f50a49dc50251be129cc83b6c472c6fed03e0e9
Matthew-19 <113844755+Matthew-19@users.noreply.github.com>
2023-06-13 20:54:11 +0300

    Candidate profile (admin) Form Validation
    No Validation for image file type yet

--- pages/personal_info/candidate_profile.js
+++ pages/personal_info/candidate_profile.js
@@ -1,20 +1,11 @@
-let myPhoto = document.querySelector("#photo");
-let myName = document.querySelector("#cName");
-let myAge = document.querySelector("#age");
-let myParty = document.querySelector("#party");
-let myedu = document.querySelector("#education");
-let sub = document.querySelector("#sub");
-console.log("sssssssssss");
-console.log(sub);
 // to upload image
 let img = document.querySelector("#photo");
 let input_image = document.querySelector("#upload");
 let uploadBtn = document.querySelector("#upload-btn");
-let reader;
 input_image.addEventListener("change", function () {
   let choosedImage = this.files[0];
   if (choosedImage) {
-    reader = new FileReader();
+    let reader = new FileReader();
     reader.addEventListener("load", function () {
       img.setAttribute("src", reader.result);
     });
@@ -22,14 +13,87 @@ input_image.addEventListener("change", function () {
   }
 });
 
-// // to create card
-sub.addEventListener("click", function () {
-  // person.name = myName.value;
-  // person.age = myAge.value;
-  // person.party = myParty.value;
-  // person.edu = myedu.value;
-  // person.photo = reader.reader;
-  all.push(2);
-  console.log("jjhjkjhoklj");
-  window.location.href = "../voting_panel/voting_panel_Admin.html";
-});
+let form = document.getElementById('data');
+let myPhoto = document.getElementById('photo');
+let myName = document.getElementById('cName');
+let myAge = document.getElementById('age');
+let myParty = document.getElementById('party');
+let myedu = document.getElementById('education');
+let sub = document.querySelector("#sub");
+
+
+//Show input error messages
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+//show success colour
+function showSucces(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'success';
+}
+
+// check Candidates Numbers
+function checkAge() {
+  let number = myAge.value;
+  if (number === '') {
+    showError(myAge,'Required');
+  }else if (number < 21) {
+    showError(myAge,'Age cant be below 21');
+  }else {
+    showSucces(myAge);
+  }
+}
+
+//check arabic letters only is valid
+function checkLetters(input) {
+  const re = /[\u0600-\u065F\u066A-\u06EF\u06FA-\u06FF]/;
+  if(re.test(input.value.trim())) {
+    showSucces(input)
+  }else if (input.value.trim() === '') {
+    showError(input,'Required');
+  }else {
+    showError(input,'Arabic letters only allowed [أ - ي]');
+  }
+}
+
+
+// Event Listeners
+// Validation
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  checkLetters(myName);
+  checkLetters(myParty);
+  checkLetters(myedu);
+  checkAge();
+})
+
+
+
+
+
+
+
+
+// console.log("sssssssssss");
+// console.log(sub);
+
+
+
+
+
+// // // to create card
+// sub.addEventListener("click", function () {
+//   // person.name = myName.value;
+//   // person.age = myAge.value;
+//   // person.party = myParty.value;
+//   // person.edu = myedu.value;
+//   // person.photo = reader.reader;
+//   all.push(2);
+//   console.log("jjhjkjhoklj");
+//   window.location.href = "../voting_panel/voting_panel_Admin.html";
+// });
