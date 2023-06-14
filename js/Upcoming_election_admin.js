45db0433b95e25403eefacee642a8d679307463b
Matthew-19 <113844755+Matthew-19@users.noreply.github.com>
2023-06-14 14:20:57 +0300

    Fixes
    ✅ Edit Election Page Created.
    ✅ Edit Election Button redirects correctly to Edit Election Page.

--- js/Upcoming_election_admin.js
+++ js/Upcoming_election_admin.js
@@ -1,13 +1,14 @@
+// Remove Election Button
 let child = document.querySelectorAll('.cancel');
 child.forEach( (e) => {
   e.onclick = function(){
     e.parentElement.parentElement.remove();
   }
 });
-// *********************************** m7taga tzbet *****************************************
+// Edit Election Button
 let ed = document.querySelectorAll('.edit');
 ed.forEach((e)=> {
   e.onclick = function(){
-    window.location.href="/pages/elections/create_election.html";
+    window.location.href="edit_election.html";
   }
 })
